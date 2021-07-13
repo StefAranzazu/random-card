2dabd1b2a286499cf84a559238c289aa22a506b9
StefAranzazu <stefanyaranzazu1310@gmail.com>
2021-07-13 16:53:24 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,41 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const symbols = ["&hearts;", "&spades;", "&clubs;", "&diams;"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomSym = symbols[Math.floor(Math.random() * symbols.length)];
+  let upSuit = document.getElementById("upSuit");
+  let downSuit = document.getElementById("downSuit");
+  upSuit.innerHTML = randomSym;
+  document.getElementById("number").innerHTML = generateRandomNumber();
+  downSuit.innerHTML = randomSym;
+
+  if (randomSym == "&hearts;" || randomSym == "&diams") {
+    upSuit.style.color = "red";
+    downSuit.style.color = "red";
+  } else {
+    upSuit.style.color = "black";
+    downSuit.style.color = "black";
+  }
 };
+
+function generateRandomNumber() {
+  let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+  return randomNum;
+}
